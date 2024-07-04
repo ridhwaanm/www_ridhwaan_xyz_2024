d42ddc834155d7aa0b7933ce78b224d66d7fb9e5
Ridhwaan Mayet <69823779+ridhwaanm@users.noreply.github.com>
2024-07-04 21:58:51 +0200

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -47,5 +47,17 @@ export default async function sitemap() {
     });
   });
 
-  return sitemapEntries;
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${sitemapEntries.map(entry => `
+  <url>
+    <loc>${entry.url}</loc>
+    <lastmod>${entry.lastModified.toISOString()}</lastmod>
+    <changefreq>${entry.changeFrequency}</changefreq>
+    <priority>${entry.priority}</priority>
+  </url>
+`).join('')}
+</urlset>`;
+
+  return sitemapXml;
 }
