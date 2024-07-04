f345b37e202b41d5da27fb554ef94c78a12c8282
ridhwaanm <ridhwaan@ridhwaan.xyz>
2024-07-04 22:12:11 +0200

    sitemapv3

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,39 +1,37 @@
-import { MetadataRoute } from 'next'
- 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default function sitemap() {
   return [
     {
-      url: 'https://ridhwaan.xyz',
+      url: 'https://www.ridhwaan.xyz',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://ridhwaan.xyz/about',
+      url: 'https://www.ridhwaan.xyz/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ridhwaan.xyz/work',
+      url: 'https://www.ridhwaan.xyz/work',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ridhwaan.xyz/process',
+      url: 'https://www.ridhwaan.xyz/process',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ridhwaan.xyz/contact',
+      url: 'https://www.ridhwaan.xyz/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://ridhwaan.xyz/blog',
+      url: 'https://www.ridhwaan.xyz/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
